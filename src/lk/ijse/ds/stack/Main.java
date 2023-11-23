7698f3e43524531d18a296cbb8edfc6d45eccfd7
danu <danujagreru@gmail.com>
2023-11-23 09:35:31 +0530

    Stack

---
+++ src/lk/ijse/ds/stack/Main.java
@@ -0,0 +1,19 @@
+package lk.ijse.ds.stack;
+
+/*
+    @author DanujaV
+    @created 11/23/23 - 9:12 AM   
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        Stack stack = new Stack(5);
+
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+
+        stack.printStack();
+
+    }
+}
---
+++ src/lk/ijse/ds/stack/Stack.java
@@ -0,0 +1,28 @@
+package lk.ijse.ds.stack;
+
+/*
+    @author DanujaV
+    @created 11/23/23 - 9:12 AM   
+*/
+
+public class Stack {
+    private int[] elementData;
+    private int top;    // points to the top of the stack(last element)
+
+    public Stack(final int initialCapacity) {
+        elementData = new int[initialCapacity];
+        top = -1;
+    }
+
+    public void push(int data) {
+        elementData[++top] = data;
+    }
+
+    public void printStack() {
+        System.out.print("[");
+        for (int i = 0; i <= top; i++) {
+            System.out.print(elementData[i] + ", ");
+        }
+        System.out.println("\b\b]");
+    }
+}
