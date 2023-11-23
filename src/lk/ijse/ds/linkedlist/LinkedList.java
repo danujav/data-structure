c692010ff1a257670c06d7f54005be749a3f5329
danu <danujagreru@gmail.com>
2023-11-23 12:44:49 +0530

    initiate LinkedList class

---
+++ src/lk/ijse/ds/linkedlist/LinkedList.java
@@ -0,0 +1,18 @@
+package lk.ijse.ds.linkedlist;
+
+/*
+    @author DanujaV
+    @created 11/23/23 - 12:41 PM   
+*/
+
+public class LinkedList {
+    private static class Node {
+        private int data;
+        private Node next;
+
+        public Node(int data) {
+            this.data = data;
+        }
+    }
+    private Node head;
+}
---
+++ src/lk/ijse/ds/linkedlist/Main.java
@@ -0,0 +1,12 @@
+package lk.ijse.ds.linkedlist;
+
+/*
+    @author DanujaV
+    @created 11/23/23 - 12:41 PM   
+*/
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
